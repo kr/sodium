7682b4a1c66e9ff4bf8cb8b750012cba70e5944e
Keith Rarick <kr@xph.us>
2007-01-08 21:56:06 -0800

    Make the sample performance test programs in C and Python match the LX version's behavior.

--- count.c
+++ count.c
@@ -5,9 +5,11 @@ main(int argc, char **argv)
 {
     int n = 0;
 
-    for (;;) {
+    for (; n < 5000000;) {
         n = n + 1;
 
         if ((n % 100000) == 0) printf("%d\n", n);
     }
+
+    return 0;
 }
--- count.py
+++ count.py
@@ -1,6 +1,5 @@
 n = 0
 
-while True:
+while n < 5000000:
     n = n + 1
-
     if (n % 100000) == 0: print n
