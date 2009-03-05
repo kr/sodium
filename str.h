9a46e7f4db079b33eb3e73426e2b0ef2ac1388cf
Keith Rarick <kr@xph.us>
2009-03-05 14:48:38 -0800

    Provide a way to allocate permanent strs.

--- str.h
+++ str.h
@@ -13,6 +13,7 @@ void str_init();
 int strp(datum x);
 
 datum make_str_init(size_t size, const char *bytes);
+datum make_str_init_permanent(size_t size, const char *bytes);
 
 size_t copy_str_contents(char *dest, datum str, size_t n);
 size_t copy_str_contents0(char *dest, datum str, size_t n);
