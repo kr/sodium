4a2aa29ffc23f1adf2bc09abcbc460c6fa12be11
Keith Rarick <kr@xph.us>
2009-03-06 00:31:19 -0800

    Refactor.

--- obj.c
+++ obj.c
@@ -11,33 +11,3 @@ closure_env(datum d)
 {
     return (datum) *d;
 }
-
-int
-closure_has_method(datum d, datum name)
-{
-    int i, n;
-    method_table table;
-
-    table = (method_table) datum_mtab(d);
-
-    n = datum2int(table->size);
-    for (i = 0; i < n; ++i) {
-        if (table->items[i].name == name) return 1;
-    }
-    return 0;
-}
-
-datum
-closure_methods(datum d)
-{
-    int i, n;
-    method_table table;
-    datum methods = nil;
-
-    /* This pointer is stable. A garbage collection will not invalidate it */
-    table = (method_table) datum_mtab(d);
-
-    n = datum2int(table->size);
-    for (i = 0; i < n; ++i) methods = cons(table->items[i].name, methods);
-    return methods;
-}
--- obj.h
+++ obj.h
@@ -6,7 +6,5 @@
 #include "mem.h"
 
 datum closure_env(datum d);
-int closure_has_method(datum d, datum name);
-datum closure_methods(datum d);
 
 #endif /*obj_h*/
